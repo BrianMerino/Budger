b16e96ea19661b548c67918ab9b1dc8f0fd277e5
Brian Merino <brianmerino08@gmail.com>
2026-02-15 13:40:20 -0500

    Added data models for transactions and budgeting categories

--- BUDGET/Models.swift
+++ BUDGET/Models.swift
@@ -11,13 +11,13 @@ import SwiftData
 @Model
 class Account{
     var id: String
-    var name: String
-    var type: AccountType
+    var name: String //EX: "Chase checking", "Savings"
+    var type: AccountType //EX: checking, saving, credit
     var balance: Double
     var lastUpdated: Date
     
     @Relationship(deleteRule: .cascade)
-    //var transactions: [Transaction] = []
+    var transactions: [Transaction] = []
     
     init(name: String, type: AccountType, balance: Double){
         self.id = UUID().uuidString
@@ -34,3 +34,50 @@ enum AccountType: String, Codable {
     case credit
     case investment
 }
+
+@Model
+class Transaction{
+    var id: String
+    var amount: Double //negative for expenses, positive for income
+    var date: Date
+    var category: String
+    var merchantName: String
+    var note: String
+    var isPending: Bool
+    
+    var account: Account?
+    
+    init(amount: Double,
+         category: String,
+         merchantName: String,
+         date: Date = Date(),
+         note: String = "",
+         isPending: Bool = false){
+        self.id = UUID().uuidString
+        self.amount = amount
+        self.category = category
+        self.merchantName = merchantName
+        self.date = date
+        self.note = note
+        self.isPending = isPending
+        }
+}
+
+@Model
+class BudgetCategory {
+    var id: String
+    var name: String
+    var budgetAmount : Double
+    var color: String
+    var icon: String
+    
+    init(name: String, budgetAmount: Double, color: String = "#007AFF", icon: String = "dollarsign.circle"){
+        self.id = UUID().uuidString
+        self.name = name
+        self.budgetAmount = budgetAmount
+        self.color = color
+        self.icon = icon
+    }
+}
+
+
